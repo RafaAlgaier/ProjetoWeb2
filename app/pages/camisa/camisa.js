af7fc89025e40382527f99979ccc18d1911d7d75
Rafael Algaier Teixeira <algaier39@gmail.com>
2025-02-23 19:46:50 -0300

    commit

--- app/pages/camisa/camisa.js
+++ app/pages/camisa/camisa.js
@@ -7,7 +7,7 @@ import { FreteService } from "../../service/freteService.js";
 document.addEventListener("DOMContentLoaded", () => {
   const consultarFreteBtn = document.getElementById("consultarFreteBtn");
   const cepInput = document.getElementById("cepInput");
-  const freteResultado = document.getElementById("freteResultado");
+  const freteResult = document.getElementById("freteResult");
 
   const freteService = new FreteService();
 
@@ -15,24 +15,24 @@ document.addEventListener("DOMContentLoaded", () => {
     const cep = cepInput.value.replace("-", "").trim();
 
     if (!/^\d+$/.test(cep)) {
-      freteResultado.innerHTML =
+      freteResult.innerHTML =
         '<p class="text-red-500">Por favor, digite apenas números para o CEP.</p>';
       return;
     }
 
     if (cep.length !== 8) {
-      freteResultado.innerHTML =
+      freteResult.innerHTML =
         '<p class="text-red-500">Por favor, digite um CEP válido com 8 dígitos.</p>';
       return;
     }
 
     try {
       const data = await freteService.buscarCep(cep);
-      freteResultado.innerHTML = `
+      freteResult.innerHTML = `
         <p><strong>A entrega para:</strong> ${data.state}, ${data.city}, ${data.neighborhood}, ${data.street}<strong> é de R$19,99</strong></p>
       `;
     } catch (error) {
-      freteResultado.innerHTML = `<p class="text-red-500">Erro: ${error.message}</p>`;
+      freteResult.innerHTML = `<p class="text-red-500">Erro: ${error.message}</p>`;
     }
   });
 });
